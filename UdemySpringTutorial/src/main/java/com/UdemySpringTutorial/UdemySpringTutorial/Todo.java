bb143a1d693f3fdc724789640efb64ebdfe7db7c
rompasaurus <michael.e.mchenry@gmail.com>
2020-06-11 15:58:42 -0400

    delete and update routes added to todoresource

--- UdemySpringTutorial/src/main/java/com/UdemySpringTutorial/UdemySpringTutorial/Todo.java
+++ UdemySpringTutorial/src/main/java/com/UdemySpringTutorial/UdemySpringTutorial/Todo.java
@@ -1,10 +1,21 @@
 package com.UdemySpringTutorial.UdemySpringTutorial;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Todo {
     private long id;
-    private String username, description;
+    private String username;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
     private Date targetDate;
     private boolean isDone;
 
@@ -46,5 +57,18 @@ public class Todo {
 
     public void setDone(boolean done) {
         isDone = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Todo todo = (Todo) o;
+        return id == todo.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- UdemySpringTutorial/src/main/java/com/UdemySpringTutorial/UdemySpringTutorial/TodoResource.java
+++ UdemySpringTutorial/src/main/java/com/UdemySpringTutorial/UdemySpringTutorial/TodoResource.java
@@ -1,12 +1,12 @@
 package com.UdemySpringTutorial.UdemySpringTutorial;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TodoResource {
     // autowired allows spring services to be instantiated
@@ -16,5 +16,19 @@ public class TodoResource {
     @GetMapping("/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username) {
         return todoService.findAll();
+    }
+
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo getTodo(@PathVariable String username,@PathVariable long id) {
+        return todoService.findById(id);
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
+        Todo todo = todoService.deleteById(id);
+        if (todo != null) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
